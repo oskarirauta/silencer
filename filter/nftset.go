9de2c2e5206b1bbe08de0b464ab99ecd58f92153
Oskari Rauta <oskari.rauta@gmail.com>
2022-03-08 00:51:02 +0200

    attempt to fix parser
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- filter/nftset.go
+++ filter/nftset.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net"
 	"os/exec"
-	"strings"
+	"github.com/buger/jsonparser"
 )
 
 type nftset struct {
@@ -38,28 +38,21 @@ func (b nftset) Unblock(ip net.IP) {
 		log.Printf("command %q failed with %q", cmd, string(output))
 	}
 }
-
+/*
 func parseNftsetList(buf []byte) (list []net.IP) {
 	for _, line := range strings.Split(string(buf), "\n") {
-		fields := strings.Fields(line)
-		if len(fields) != 3 {
-			continue
-		}
-		if fields[0] != "add" {
-			continue
-		}
-		ip := net.ParseIP(fields[2]).To4()
+		ip := net.ParseIP(line).To4()
 		if ip == nil {
-			log.Printf("invalid IPv4 address: %q", fields[2])
+			log.Printf("invalid IPv4 address: %q", line)
 			continue
 		}
 		list = append(list, ip)
 	}
 	return
 }
-
+*/
 func (b nftset) List() []net.IP {
-	cmd := exec.Command("nft", "list", "set", "inet", "fw4", b.set)
+	cmd := exec.Command("nft", "--json", "list", "set", "inet", "fw4", b.set)
 	//cmd := exec.Command("ipset", "list", b.set, "-output", "save")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -67,5 +60,38 @@ func (b nftset) List() []net.IP {
 		return nil
 	}
 
-	return parseNftsetList(output)
+	if parsed, _, _, err := jsonparser.Get(output, "nftables"); err == nil {
+		output = parsed
+	} else {
+		return nil
+	}
+
+	var elemlist []net.IP
+	success := false
+
+	jsonparser.ArrayEach(output, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		if setdata, _, _, err := jsonparser.Get(value, "set"); err == nil {
+			if elements, _, _, err := jsonparser.Get(setdata, "elem"); err == nil {
+				var elemlist []net.IP
+				jsonparser.ArrayEach(elements, func(value2 []byte, dataType2 jsonparser.ValueType, offset2 int, err2 error) {
+					if dataType2 == jsonparser.String {
+						if str, err := jsonparser.ParseString(value2); err == nil {
+							if ip := net.ParseIP(str).To4(); ip != nil {
+								elemlist = append(elemlist, ip)
+							} else {
+								log.Printf("invalid IPv4 address: %q", str)
+							}
+						}
+					}
+				success = true
+				})
+			}
+		}
+	})
+
+	if success {
+		return elemlist
+	}
+
+	return nil
 }
